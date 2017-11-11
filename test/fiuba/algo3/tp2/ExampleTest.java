f78ca101290a290ff82dab774c074026b43be4f8
dsesposito <dsesposito@debmedia.com>
2017-11-10 22:23:11 -0300

    Se agrega manejador de dependencias y compilador.
    Se agrega test ejemplo

--- test/fiuba/algo3/tp2/ExampleTest.java
+++ test/fiuba/algo3/tp2/ExampleTest.java
@@ -1,4 +1,12 @@
 package fiuba.algo3.tp2;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class ExampleTest {
+
+    @Test
+    public void test01AgregarProductos() {
+        String result = "Foo";
+        Assert.assertEquals("Foo", result);
+    }
 }
