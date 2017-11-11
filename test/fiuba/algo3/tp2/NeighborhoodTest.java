7a651cb386a41d87dd8944bca4dbf11308130ffc
dsesposito <dsesposito@debmedia.com>
2017-11-11 17:20:17 -0300

    Se borran archivos de mas
    Se agrega el @Test al test de barrio.

--- test/fiuba/algo3/tp2/NeighborhoodTest.java
+++ test/fiuba/algo3/tp2/NeighborhoodTest.java
@@ -3,9 +3,11 @@ package fiuba.algo3.tp2;
 import fiuba.algo3.tp2.model.Neighborhood;
 import fiuba.algo3.tp2.model.Player;
 import org.junit.Assert;
+import org.junit.Test;
 
 public class NeighborhoodTest {
 
+    @Test
     public void testBuyNeighborhoodHeMustBeTheOwner(){
         Player player1 = new Player("Diego");
         Neighborhood bsassur = new Neighborhood("Buenos Aires - Sur");
