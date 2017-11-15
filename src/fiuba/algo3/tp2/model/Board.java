a60e01f9e06366121a858745f7ea3473bc45b8b3
Guido <gdipaolo@sysgarage.com>
2017-11-15 11:41:37 -0300

    Delete players from board

--- src/fiuba/algo3/tp2/model/Board.java
+++ src/fiuba/algo3/tp2/model/Board.java
@@ -1,7 +1,6 @@
 package fiuba.algo3.tp2.model;
 
 import fiuba.algo3.tp2.Global;
-import fiuba.algo3.tp2.model.Exceptions.BoardPlayerQuantityException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,13 +8,9 @@ import java.util.List;
 public class Board {
 
     List<Cell> cells;
-    List<Player> players;
-
-    private static Long maxNumberOfPlayersOnBoard = Global.config.getLong("maxNumberOfPlayersOnBoard");
 
     public Board() {
         cells = new ArrayList<>();
-        players = new ArrayList<>();
         String[] cellsNames = Global.config.getString("board.cells").split(",");
         int position = 0;
         for(String cellName : cellsNames) {
@@ -26,21 +21,5 @@ public class Board {
 
     public List getCells(){
         return this.cells;
-    }
-
-    public Boolean isAbleToAddPlayer(){
-        return (getQuantityOfPlayers() <= maxNumberOfPlayersOnBoard);
-    }
-
-    public Integer getQuantityOfPlayers(){
-        return players.size();
-    }
-
-    public void addPlayerToBoard(Player player){
-        if(!isAbleToAddPlayer()){
-            throw new BoardPlayerQuantityException("The maximium of three players have already been reached");
-        }
-        players.add(player);
-
     }
 }
--- test/fiuba/algo3/tp2/BoardTest.java
+++ test/fiuba/algo3/tp2/BoardTest.java
@@ -1,8 +1,6 @@
 package fiuba.algo3.tp2;
 
 import fiuba.algo3.tp2.model.Board;
-import fiuba.algo3.tp2.model.Exceptions.BoardPlayerQuantityException;
-import fiuba.algo3.tp2.model.Player;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -21,37 +19,5 @@ public class BoardTest {
 
         Assert.assertTrue(board.getCells().size() == boardTotalCells);
 
-    }
-
-    @Test
-    public void test02AddThreePlayersToBoard(){
-        Board board = new Board();
-
-        Player player1 = new Player("Lucas");
-        Player player2 = new Player("Diego");
-        Player player3 = new Player("Guido");
-
-        board.addPlayerToBoard(player1);
-        board.addPlayerToBoard(player2);
-        board.addPlayerToBoard(player3);
-
-        Assert.assertTrue(board.getQuantityOfPlayers() == 3);
-    }
-
-    @Test
-    public void test03AddFourPlayersToBoard(){
-        Board board = new Board();
-
-        Player player1 = new Player("Lucas");
-        Player player2 = new Player("Diego");
-        Player player3 = new Player("Guido");
-        Player player4 = new Player("Gonzalo");
-
-        board.addPlayerToBoard(player1);
-        board.addPlayerToBoard(player2);
-        board.addPlayerToBoard(player3);
-
-        thrown.expect(BoardPlayerQuantityException.class);
-        board.addPlayerToBoard(player4);
     }
 }
